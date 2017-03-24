0e3956bb3407e23789f1564af9c124707370c73b
Ivan Baranov <baranov.ivan.v@gmail.com>
2017-03-24 18:48:35 +0300

    Add basic functions.

--- basic_functions.lisp
+++ basic_functions.lisp
@@ -1,8 +1,12 @@
 ;;;; Basic functions.
 
-;;; CAR function.
 
-;; (car list) -> s-expression. Returns `head` of the list.
+;;; CAR/FIRST and CDR/REST functions.
+;;; The difference between equivalents http://stackoverflow.com/questions/29907440/difference-between-cdr-car-and-rest-first-and-possible-implementation
+
+;;; CAR/FIRST function.
+;;; (car list) -> s-expression. Returns `head` of the list.
+
 ;; Returns 1
 (print (car '(1 2 3)))
 
@@ -13,7 +17,116 @@
 (print (car nil))
 
 ;; Returns NIL
-(print (car 'nil))
+(print (first 'nil))
 
 ;; Returns NIL
-(print (car '(nil 1 2 3)))
+(print (first '(nil 1 2 3)))
+
+;;; CDR/REST function.
+;;; (cdr list) -> s-expression. Returns `tail` of the list (that is everything except `head`).
+
+;; Returns (1 2 3)
+(print (cdr '(nil 1 2 3)))
+
+;; Returns REST
+(print (first '(rest '(1 2 3))))
+
+;;; CONS function. Adds `head` to `tail`.
+
+;; Returns (1 2 3)
+(print (cons 1 '(2 3)))
+
+;; Returns (5 2 3)
+(print (cons (+ 1 4) '(2 3)))
+
+;; Returns ((+ 1 4) 2 3)
+(print (cons '(+ 1 4) '(2 3)))
+
+;; Returns (1 2 3)
+(print (cons 1 (cons 2 (cons 3 nil))))
+
+;; Returns (NIL). NIL is not the same as (NIL). (NIL) is a list with one element.
+(print (cons nil nil))
+
+
+;;; Predicate functions. Predicates return T(or any value except NIL) or NIL as a result.
+
+;;; ATOM predicate returns if expression is an atom. (ATOM s-expression) -> T/NIL
+
+;; Returns T
+(print (atom 1))
+
+;; Returns T
+(print (atom 'a))
+
+;; Returns T
+(print (atom nil))
+
+;; Returns T
+(print (atom ()))
+
+;; Returns T
+(print (atom '()))
+
+;; Returns T
+(print (atom 'nil))
+
+;; Returns T
+(print (atom (+ 1 2)))
+
+;;; EQ predicate returns T if arguments are equal. Arguments can be symbols or constants T and NIL.
+
+;; Returns T
+(print (eq nil ()))
+
+;; Returns T
+(print (eq nil 'nil))
+
+;; Returns NIL
+(print (eq 'a 'b))
+
+;; Returns NIL
+(print (eq '(1 2) ' (1 2)))
+
+;; Returns NIL
+(print (eq 3.14 3.14))
+
+;;; EQL predicate also compares numbers of the same type.
+
+;; Returns T
+(print (eql 3.14 3.14))
+
+;; Returns NIL
+(print (eql 3 3.14))
+
+;;; = predicate compares numbers of different type.
+
+;; Returns T
+(print (= 3 3.0))
+
+;; Returns T
+(print (= 3 0.3e1))
+
+;;; EQUAL predicate is like EQL, but also works with lists.
+
+;; Returns T
+(print (equal '(1 2 3) '(1 2 3)))
+
+;; Returns T
+(print (equal 'a 'a))
+
+;;; EQUALP predicate. Compares numbers of different types and is case insensitive while comparing chars and strings.
+
+;; Returns T
+(print (equalp "Foo" "FOo"))
+
+;; Returns T
+(print (equalp '(1 2.0) '(1 2)))
+
+;;; NULL predicate checks if list is empty.
+
+;; Returns NIL
+(print (null (1 2 3))
+
+;; Returns T
+(print (null ())
